a99c684fa29a57124d1ff57281e6d2ad66a59f96
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-07 20:41:19 +0700

    membuat HalamanListPeserta composable screen

---
+++ app/src/main/java/com/example/tugaspratikum6/view/HalamanListPeserta.kt
@@ -0,0 +1,24 @@
+package com.example.tugaspratikum6.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanListPeserta(
+    pesertaList: List<PesertaData>,
+    onFormClick: () -> Unit,
+    onHomeClick: () -> Unit
+) {
+
+}
