125e5805d57d9ff9565ed261f57befbc1bf7e46c
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-07 20:36:41 +0700

    membuat HalamanWelcome composable dengan gambar dan layout kolom

---
+++ app/src/main/java/com/example/tugaspratikum6/view/HalamanWelcome.kt
@@ -0,0 +1,26 @@
+package com.example.tugaspratikum6.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugaspratikum6.R
+
+@Composable
+fun HalamanWelcome(onNextClick: () -> Unit) {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+        }
